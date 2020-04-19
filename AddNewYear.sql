b29e2a0f17652e4463bda7decabfdb212701e0bf
ofariz <oguzhanfariz@gmail.com>
2020-04-19 21:37:16 +0300

    Update AddNewYear.sql

--- AddNewYear.sql
+++ AddNewYear.sql
@@ -1,20 +1,15 @@
 DECLARE @YearPartition varchar(10)='2020'
+DECLARE @DBNAME varchar(50) ='DBNAME'
+DECLARE @SCHEMA_NAME varchar(50) ='SCHEMA_NAME'
+DECLARE @PARTITION_FUNCTION_NAME varchar(50) ='PARTITION_FUNCTION_NAME'
 
+print('USE [master]
+GO
+ALTER DATABASE ['+@DBNAME+'] ADD FILEGROUP [Part_'+@YearPartition+']
+ALTER DATABASE ['+@DBNAME+'] ADD FILE ( NAME = N''Part_'+@YearPartition+''', FILENAME = N''D:\MSSQL\MSSQL\DATA\'+@DBNAME+'_PART_'+@YearPartition+'.ndf'' , SIZE = 10MB , FILEGROWTH = 10MB ) TO FILEGROUP [Part_'+@YearPartition+']')
 
-print('ALTER DATABASE [PartitioningTest] ADD FILEGROUP [LOG_'+@YearPartition+']
-ALTER DATABASE [PartitioningTest] ADD FILE ( NAME = N''LOG_'+@YearPartition+''', FILENAME = N''C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\PartitioningTest_LOG'+@YearPartition+'.ndf'' , SIZE = 10MB , FILEGROWTH = 10MB ) TO FILEGROUP [LOG_'+@YearPartition+']')
-print('Alter Partition Scheme LOG_PartitionScheme_Small NEXT USED [LOG_'+@YearPartition+']')
-print('ALTER PARTITION FUNCTION YearlyPartitionWithoutCurentYear()  
-SPLIT RANGE (N'''+@YearPartition+'-01-01'')')
-
-print('Alter Partition Scheme LOG_PartitionScheme_Small NEXT USED [LOG_'+@YearPartition+']')
-print('ALTER PARTITION FUNCTION YearlyPartitionWithoutCurentYear()  
-SPLIT RANGE (N'''+@YearPartition+'-03-01'')')
-
-print('Alter Partition Scheme LOG_PartitionScheme_Small NEXT USED [LOG_'+@YearPartition+']')
-print('ALTER PARTITION FUNCTION YearlyPartitionWithoutCurentYear()  
-SPLIT RANGE (N'''+@YearPartition+'-06-01'')')
-
-print('Alter Partition Scheme LOG_PartitionScheme_Small NEXT USED [LOG_'+@YearPartition+']')
-print('ALTER PARTITION FUNCTION YearlyPartitionWithoutCurentYear()  
-SPLIT RANGE (N'''+@YearPartition+'-09-01'')')
+print('USE ['+@DBNAME+']
+GO')
+print('Alter Partition Scheme ['+@SCHEMA_NAME+'] NEXT USED [Part_'+@YearPartition+']')
+print('ALTER PARTITION FUNCTION '+@PARTITION_FUNCTION_NAME+'() 
+SPLIT RANGE (N'''+@YearPartition+'-31-01'')')
